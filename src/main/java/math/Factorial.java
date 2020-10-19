6dc72cfec9040ee52cf327ff61c290d8de8b07e8
Marvin Himmelmeier <mail@marvinhi.de>
2020-10-19 18:57:25 +0200

    Added recursive factorial method with tests

--- src/main/java/math/Factorial.java
+++ src/main/java/math/Factorial.java
@@ -4,6 +4,8 @@ import java.math.BigInteger;
 
 public class Factorial {
 
+    private static final String NEGATIVE_ARG_EXCEPTION = "n must be greater than -1!";
+
     /**
      * Factorial-method using BigInteger which can hold values up to 2^Integer.MAX_VALUE. The long datatype can only
      * be used for factorials with n < 21.
@@ -13,12 +15,25 @@ public class Factorial {
      */
     public static BigInteger factorial(int n) {
         if (n < 0) {
-            throw new IllegalArgumentException("n must be greater than -1!");
+            throw new IllegalArgumentException(NEGATIVE_ARG_EXCEPTION);
         }
         BigInteger fac = BigInteger.ONE;
         for (int i = 2; i <= n; i++) {
             fac = fac.multiply(BigInteger.valueOf(i));
         }
         return fac;
+    }
+
+    /**
+     * Like {@link #factorial(int)} just with recursion.
+     *
+     * @param n Integer to which the factorial shall be calculated
+     * @return Factorial of n
+     */
+    public static BigInteger factorialRec(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException(NEGATIVE_ARG_EXCEPTION);
+        }
+        return n <= 1 ? BigInteger.ONE : BigInteger.valueOf(n).multiply(factorialRec(n - 1));
     }
 }
--- src/test/java/math/FactorialTest.java
+++ src/test/java/math/FactorialTest.java
@@ -13,6 +13,11 @@ public class FactorialTest {
         Factorial.factorial(-1);
     }
 
+    @Test(expected = IllegalArgumentException.class)
+    public void testNegativeValuesRec() {
+        Factorial.factorialRec(-1);
+    }
+
     @Test
     public void testSimpleFactorials() {
         assertEquals("0! should be 1.", BigInteger.ONE, Factorial.factorial(0));
@@ -21,6 +26,16 @@ public class FactorialTest {
         assertEquals("3! should be 6.", BigInteger.valueOf(6), Factorial.factorial(3));
         assertEquals("4! should be 24.", BigInteger.valueOf(24), Factorial.factorial(4));
         assertEquals("5! should be 120.", BigInteger.valueOf(120), Factorial.factorial(5));
+    }
+
+    @Test
+    public void testSimpleFactorialsRec() {
+        assertEquals("0! should be 1.", BigInteger.ONE, Factorial.factorialRec(0));
+        assertEquals("1! should be 1.", BigInteger.ONE, Factorial.factorialRec(1));
+        assertEquals("2! should be 2.", BigInteger.valueOf(2), Factorial.factorialRec(2));
+        assertEquals("3! should be 6.", BigInteger.valueOf(6), Factorial.factorialRec(3));
+        assertEquals("4! should be 24.", BigInteger.valueOf(24), Factorial.factorialRec(4));
+        assertEquals("5! should be 120.", BigInteger.valueOf(120), Factorial.factorialRec(5));
     }
 
     @Test
@@ -41,5 +56,25 @@ public class FactorialTest {
         factorialNumber = "8320987112741390144276341183223364380754172606361245952449277696409600000000000000";
         assertEquals(n + "! should be " + factorialNumber + ".",
                 new BigInteger(factorialNumber), Factorial.factorial(n));
+    }
+
+    @Test
+    public void testBigFactorialsRec() {
+        int n = 30;
+        String factorialNumber = "265252859812191058636308480000000";
+        assertEquals(n + "! should be " + factorialNumber + ".",
+                new BigInteger(factorialNumber), Factorial.factorialRec(n));
+        n = 40;
+        factorialNumber = "815915283247897734345611269596115894272000000000";
+        assertEquals(n + "! should be " + factorialNumber + ".",
+                new BigInteger(factorialNumber), Factorial.factorialRec(n));
+        n = 50;
+        factorialNumber = "30414093201713378043612608166064768844377641568960512000000000000";
+        assertEquals(n + "! should be " + factorialNumber + ".",
+                new BigInteger(factorialNumber), Factorial.factorialRec(n));
+        n = 60;
+        factorialNumber = "8320987112741390144276341183223364380754172606361245952449277696409600000000000000";
+        assertEquals(n + "! should be " + factorialNumber + ".",
+                new BigInteger(factorialNumber), Factorial.factorialRec(n));
     }
 }
