34d1499069e65b87d273936e4f973176628b5900
Marvin Himmelmeier <mail@marvinhi.de>
2020-10-27 19:16:51 +0100

    Added more test for factorials

--- src/test/java/math/FactorialTest.java
+++ src/test/java/math/FactorialTest.java
@@ -40,6 +40,10 @@ public class FactorialTest {
         assertEquals("3! should be 6.", BigInteger.valueOf(6), factorial.apply(3));
         assertEquals("4! should be 24.", BigInteger.valueOf(24), factorial.apply(4));
         assertEquals("5! should be 120.", BigInteger.valueOf(120), factorial.apply(5));
+        assertEquals("6! should be 720.", BigInteger.valueOf(720), factorial.apply(6));
+        assertEquals("7! should be 5,040.", BigInteger.valueOf(5040), factorial.apply(7));
+        assertEquals("8! should be 40,320.", BigInteger.valueOf(40320), factorial.apply(8));
+        assertEquals("9! should be 362,880.", BigInteger.valueOf(362880), factorial.apply(9));
     }
 
     @Test
@@ -53,8 +57,12 @@ public class FactorialTest {
     }
 
     private void bigFactorials(Function<Integer, BigInteger> factorial) {
-        int n = 30;
-        String factorialNumber = "265252859812191058636308480000000";
+        int n = 20;
+        String factorialNumber = "2432902008176640000";
+        assertEquals(n + "! should be " + factorialNumber + ".",
+                new BigInteger(factorialNumber), factorial.apply(n));
+        n = 30;
+        factorialNumber = "265252859812191058636308480000000";
         assertEquals(n + "! should be " + factorialNumber + ".",
                 new BigInteger(factorialNumber), factorial.apply(n));
         n = 40;
@@ -67,6 +75,18 @@ public class FactorialTest {
                 new BigInteger(factorialNumber), factorial.apply(n));
         n = 60;
         factorialNumber = "8320987112741390144276341183223364380754172606361245952449277696409600000000000000";
+        assertEquals(n + "! should be " + factorialNumber + ".",
+                new BigInteger(factorialNumber), factorial.apply(n));
+        n = 70;
+        factorialNumber = "11978571669969891796072783721689098736458938142546425857555362864628009582789845319680000000000000000";
+        assertEquals(n + "! should be " + factorialNumber + ".",
+                new BigInteger(factorialNumber), factorial.apply(n));
+        n = 80;
+        factorialNumber = "71569457046263802294811533723186532165584657342365752577109445058227039255480148842668944867280814080000000000000000000";
+        assertEquals(n + "! should be " + factorialNumber + ".",
+                new BigInteger(factorialNumber), factorial.apply(n));
+        n = 90;
+        factorialNumber = "1485715964481761497309522733620825737885569961284688766942216863704985393094065876545992131370884059645617234469978112000000000000000000000";
         assertEquals(n + "! should be " + factorialNumber + ".",
                 new BigInteger(factorialNumber), factorial.apply(n));
     }
